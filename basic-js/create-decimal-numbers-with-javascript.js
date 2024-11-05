a392011891fde04fbc44c9fe0408443f68feda2d
dariantonova <186691008+dariantonova@users.noreply.github.com>
2024-11-05 12:52:18 +0300

    Create Decimal Numbers with JavaScript

---
+++ basic-js/create-decimal-numbers-with-javascript.js
@@ -0,0 +1,8 @@
+// ❓ DESCRIPTION
+// Create a variable myDecimal and give it a decimal value with a fractional part (e.g. 5.7).
+
+// ✅ SOLUTION
+const ourDecimal = 5.7;
+
+// Only change code below this line
+const myDecimal = 5.8;
