b1471a073537ae54619ab7ce1bb2acf05b89d2d0
dariantonova <186691008+dariantonova@users.noreply.github.com>
2024-11-06 16:50:02 +0300

    Modify Array Data With Indexes

---
+++ basic-js/modify-array-data-with-indexes.js
@@ -0,0 +1,9 @@
+// ❓ DESCRIPTION
+// Modify the data stored at index 0 of myArray to a value of 45.
+
+// ✅ SOLUTION
+// Setup
+const myArray = [18, 64, 99];
+
+// Only change code below this line
+myArray[0] = 45;
