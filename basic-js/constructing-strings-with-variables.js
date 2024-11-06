1780d96079722010ebb73399e6a64f20f45cc74d
dariantonova <186691008+dariantonova@users.noreply.github.com>
2024-11-06 15:48:16 +0300

    Constructing Strings with Variables

---
+++ basic-js/constructing-strings-with-variables.js
@@ -0,0 +1,8 @@
+// ❓ DESCRIPTION
+// Set myName to a string equal to your name and build myStr with myName
+// between the strings My name is  and  and I am well!
+
+// ✅ SOLUTION
+// Only change code below this line
+const myName = "Daria";
+const myStr = "My name is " + myName + " and I am well!";
