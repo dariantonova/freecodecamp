8e14eac914c65fe3ec41ef53ccba21966282bc61
dariantonova <186691008+dariantonova@users.noreply.github.com>
2024-11-06 16:07:46 +0300

    Understand String Immutability

---
+++ basic-js/understand-string-immutability.js
@@ -0,0 +1,13 @@
+// ❓ DESCRIPTION
+// Correct the assignment to myStr so it contains the string value of
+// Hello World using the approach shown in the example above.
+// let myStr = "Bob";
+// myStr = "Job";
+
+// ✅ SOLUTION
+// Setup
+let myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
