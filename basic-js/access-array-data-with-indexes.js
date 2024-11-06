3ed77340d83148d2eb352fc92dbaa02fc990d8eb
dariantonova <186691008+dariantonova@users.noreply.github.com>
2024-11-06 16:48:27 +0300

    Access Array Data with Indexes

---
+++ basic-js/access-array-data-with-indexes.js
@@ -0,0 +1,7 @@
+// ❓ DESCRIPTION
+// Create a variable called myData and set it to equal the first value of myArray using bracket notation.
+
+// ✅ SOLUTION
+const myArray = [50, 60, 70];
+
+const myData = myArray[0];
