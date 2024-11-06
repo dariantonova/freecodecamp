70474de49dc4dca84a7067c29f200a69449384c9
dariantonova <186691008+dariantonova@users.noreply.github.com>
2024-11-06 15:20:34 +0300

    Compound Assignment With Augmented Subtraction

---
+++ basic-js/compound-assignment-with-augmented-subtraction.js
@@ -0,0 +1,12 @@
+// ❓ DESCRIPTION
+// Convert the assignments for a, b, and c to use the -= operator.
+
+// ✅ SOLUTION
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
