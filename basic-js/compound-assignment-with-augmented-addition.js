22e812ae0625d2630f5aeb9d8ea917f57bc8f1e3
dariantonova <186691008+dariantonova@users.noreply.github.com>
2024-11-06 15:18:19 +0300

    Compound Assignment With Augmented Addition

---
+++ basic-js/compound-assignment-with-augmented-addition.js
@@ -0,0 +1,12 @@
+// ❓ DESCRIPTION
+// Convert the assignments for a, b, and c to use the += operator.
+
+// ✅ SOLUTION
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
