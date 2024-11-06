8179f4f84a136f44f88715dd1d673b8278a4fa32
dariantonova <186691008+dariantonova@users.noreply.github.com>
2024-11-06 16:10:35 +0300

    Use Bracket Notation to Find the Nth Character in a String

---
+++ basic-js/use-bracket-notation-to-find-the-nth-character-in-a-string.js
@@ -0,0 +1,10 @@
+// ❓ DESCRIPTION
+// Let's try to set thirdLetterOfLastName to equal the third letter of the
+// lastName variable using bracket notation.
+
+// ✅ SOLUTION
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
