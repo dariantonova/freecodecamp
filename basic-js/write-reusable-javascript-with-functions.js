e74da050410ed3a08a441643145c4fb442485de4
dariantonova <186691008+dariantonova@users.noreply.github.com>
2024-11-07 10:19:27 +0300

    Write Reusable JavaScript with Functions

---
+++ basic-js/write-reusable-javascript-with-functions.js
@@ -0,0 +1,10 @@
+// ❓ DESCRIPTION
+// Create a function called reusableFunction which prints the string Hi World to the dev console.
+// Call the function.
+
+// ✅ SOLUTION
+function reusableFunction() {
+    console.log('Hi World');
+}
+
+reusableFunction();
