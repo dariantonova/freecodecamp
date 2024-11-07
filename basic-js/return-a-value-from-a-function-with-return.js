b8d00e5f6a26898fa8cda7d59dfde6b467d51128
dariantonova <186691008+dariantonova@users.noreply.github.com>
2024-11-07 10:25:45 +0300

    Return a Value from a Function with Return

---
+++ basic-js/return-a-value-from-a-function-with-return.js
@@ -0,0 +1,7 @@
+// ❓ DESCRIPTION
+// Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value.
+
+// ✅ SOLUTION
+function timesFive(num) {
+    return num * 5;
+}
