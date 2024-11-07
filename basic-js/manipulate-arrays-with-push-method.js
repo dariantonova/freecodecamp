0738448bd388fafd9f9c6c0031f9783e14014eff
dariantonova <186691008+dariantonova@users.noreply.github.com>
2024-11-07 10:02:33 +0300

    Manipulate Arrays With push Method

---
+++ basic-js/manipulate-arrays-with-push-method.js
@@ -0,0 +1,9 @@
+// ❓ DESCRIPTION
+// Push ["dog", 3] onto the end of the myArray variable.
+
+// ✅ SOLUTION
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+myArray.push(["dog", 3]);
