6d051998871060cb6edabd18bdc7230390bf4455
dariantonova <186691008+dariantonova@users.noreply.github.com>
2024-11-08 08:46:51 +0300

    Generate Random Fractions with JavaScript

---
+++ basic-js/generate-random-fractions-with-javascript.js
@@ -0,0 +1,12 @@
+// ❓ DESCRIPTION
+// Change randomFraction to return a random number instead of returning 0.
+
+// ✅ SOLUTION
+function randomFraction() {
+
+    // Only change code below this line
+
+    return Math.random();
+
+    // Only change code above this line
+}
