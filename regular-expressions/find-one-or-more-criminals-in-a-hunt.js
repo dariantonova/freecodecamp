afcfa541ce252eaea41bf90315876a5bda5a7680
dariantonova <186691008+dariantonova@users.noreply.github.com>
2024-11-09 15:25:13 +0300

    Find One or More Criminals in a Hunt

---
+++ regular-expressions/find-one-or-more-criminals-in-a-hunt.js
@@ -0,0 +1,6 @@
+// ❓ DESCRIPTION
+// Write a greedy regex that finds one or more criminals within a group of other people.
+// A criminal is represented by the capital letter C.
+
+// ✅ SOLUTION
+let reCriminals = /C+/; // Change this line
