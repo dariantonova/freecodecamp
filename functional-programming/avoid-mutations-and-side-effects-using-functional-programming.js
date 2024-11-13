970cd27c1f82803a27f5ba895394652e3ebe32f9
dariantonova <186691008+dariantonova@users.noreply.github.com>
2024-11-13 09:02:06 +0300

    Avoid Mutations and Side Effects Using Functional Programming

---
+++ functional-programming/avoid-mutations-and-side-effects-using-functional-programming.js
@@ -0,0 +1,16 @@
+// ❓ DESCRIPTION
+/*
+Fill in the code for the function incrementer so it returns the value of the global variable
+fixedValue increased by one.
+ */
+
+// ✅ SOLUTION
+// The global variable
+let fixedValue = 4;
+
+function incrementer() {
+    // Only change code below this line
+    return fixedValue + 1;
+
+    // Only change code above this line
+}
